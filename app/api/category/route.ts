d34ae51796125e0de877d745f38c0775e32da3ec
Sergio Gregorutti <sergiogregorutti@gmail.com>
2024-03-13 15:34:13 -0300

    Start API development

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -17,38 +17,3 @@ export async function GET() {
     );
   }
 }
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { method } = req;
-
-  await dbConnect();
-
-  switch (method) {
-    case "GET":
-      try {
-        const categories = await Category.find(
-          {}
-        ); /* find all the data in our database */
-        res.status(200).json({ success: true, data: categories });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    /*case "POST":
-      try {
-        const pet = await Pet.create(
-          req.body
-        );
-        res.status(201).json({ success: true, data: pet });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;*/
-    default:
-      res.status(400).json({ success: false });
-      break;
-  }
-}
