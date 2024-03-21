5f4aa003bab1f0e14ab019871e42ea4f65ab93be
Sergio Gregorutti <sergiogregorutti@gmail.com>
2024-03-20 22:49:35 -0300

    Start new 2024 version

--- app/categories/[id]/page.tsx
+++ app/categories/[id]/page.tsx
@@ -1,14 +1,27 @@
+import Link from "next/link";
 import dbConnect from "../../../lib/dbConnect";
 import Category from "../../../models/Category";
-import Layout from "../../../components/layout/Layout";
-import CategoryComponent from "../../../components/category/category";
+import Course from "../../../models/Course";
 
 async function getCategory(id: String) {
   await dbConnect();
 
-  const category = await Category.findById(id).exec();
+  const category = await Category.findById(id, "name _id").exec();
 
   return category;
+}
+
+async function getCourses(id: String) {
+  await dbConnect();
+
+  const courses = await Course.paginate(
+    { category: id },
+    {
+      select: "-photo -category",
+    }
+  );
+
+  return courses;
 }
 
 export default async function CategoryPage({
@@ -17,9 +30,25 @@ export default async function CategoryPage({
   params: { id: string };
 }) {
   const categoryData = await getCategory(params.id);
+  const courses = await getCourses(params.id);
+
   return (
-    <Layout>
-      <CategoryComponent category={JSON.parse(JSON.stringify(categoryData))} />
-    </Layout>
+    <>
+      <Link href={`/`}>Go Back</Link>
+      <h1>{categoryData.name}</h1>
+      <h2>Courses</h2>
+      <ul>
+        {courses.docs.map((course: any) => (
+          <li key={course.name}>
+            <strong>{course.name}</strong>
+            <br />
+            {course.description}
+            <br />
+            Pricing: {course.pricing} | Price: {course.price} | Platform:{" "}
+            {course.platform} | Author: {course.author} | Year: {course.year}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import Link from "next/link";
 import dbConnect from "../lib/dbConnect";
 import Category from "../models/Category";
-import Layout from "../components/layout/Layout";
-import HomeImage from "../components/HomeImage";
-import { Container, Grid, Typography, Button } from "@mui/material";
 
 async function getCategories() {
   await dbConnect();
@@ -22,122 +19,19 @@ export default async function Home() {
   const categories = await getCategories();
 
   return (
-    <Layout>
-      <Grid
-        container
-        justifyContent="center"
-        spacing={2}
-        sx={{
-          paddingTop: "40px",
-          paddingBottom: "40px",
-          flexDirection: { xs: "column-reverse", sm: "row" },
-          alignItems: { xs: "center", sm: "normal" },
-        }}
-      >
-        <Grid
-          item
-          sm={6}
-          sx={{
-            paddingLeft: { xs: "0 !important", sm: "16px !important" },
-            paddingTop: { xs: "0 !important", sm: "16px !important" },
-            display: { xs: "block", sm: "flex" },
-            alignItems: { xs: "top", sm: "center" },
-          }}
-        >
-          <Container
-            sx={{
-              padding: { sm: "0 !important" },
-              textAlign: { xs: "center", sm: "left" },
-            }}
-          >
-            <Typography
-              variant="h1"
-              component="div"
-              sx={{ fontSize: { xs: "34px", lg: "63px" } }}
-            >
-              Welcome to
-            </Typography>
-            <img src="../assets/logo.svg" style={{ width: "78%" }} />
-            <Typography
-              variant="subtitle1"
-              gutterBottom
-              component="div"
-              sx={{
-                marginTop: "-5px",
-                fontSize: { xs: "16px", sm: "18px", lg: "20px" },
-              }}
-            >
-              Course directory for developers.
-            </Typography>
-          </Container>
-        </Grid>
-        <Grid
-          item
-          sm={6}
-          sx={{
-            paddingLeft: { xs: "0 !important", lg: "16px !important" },
-            paddingTop: { xs: "0 !important", lg: "16px !important" },
-          }}
-        >
-          <HomeImage />
-        </Grid>
-      </Grid>
+    <>
+      <h1>DevsLearning</h1>
+      <p>Course directory for developers</p>
 
-      <div
-        style={{
-          borderRadius: "10px",
-          padding: "20px",
-          background: "#f7f7f7",
-        }}
-      >
-        <Typography
-          variant="h2"
-          gutterBottom
-          component="div"
-          sx={{
-            textAlign: "center",
-            marginBottom: { xs: "25px", sm: "40px" },
-            fontSize: { xs: "28px", sm: "40px" },
-          }}
-        >
-          What do you want to learn?
-        </Typography>
+      <h2>Categories</h2>
 
-        <Grid container spacing={4}>
-          {categories &&
-            categories.map((category: any) => (
-              <Grid item key={category.name} xs={12} sm={6} lg={3}>
-                <Link
-                  href={`/categories/${category._id}`}
-                  style={{ textDecoration: "none" }}
-                >
-                  <Button
-                    variant="outlined"
-                    sx={{
-                      position: "relative",
-                      fontSize: "20px",
-                      textAlign: "center",
-                      width: "100%",
-                      padding: "13px 10px 13px 42px",
-                      " img": {
-                        position: "absolute",
-                        top: "12px",
-                        left: "13px",
-                      },
-                    }}
-                  >
-                    <img
-                      src={`/api/category/photo/${category._id}`}
-                      alt={category.name}
-                      style={{ height: "38px", marginRight: "10px" }}
-                    />
-                    {category.name}
-                  </Button>
-                </Link>
-              </Grid>
-            ))}
-        </Grid>
-      </div>
-    </Layout>
+      <ul>
+        {categories.map((category: any) => (
+          <li key={category.name}>
+            <Link href={`/categories/${category._id}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
