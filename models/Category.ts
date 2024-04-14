e2b9357693c97c1dab08a73d1d779cdfa7de0bb9
Sergio Gregorutti <sergiogregorutti@gmail.com>
2024-04-14 14:46:00 -0300

    Update Category schema

--- models/Category.ts
+++ models/Category.ts
@@ -1,23 +1,21 @@
 import mongoose from "mongoose";
 
-export interface Categories extends mongoose.Document {
-  name: string;
-  photo: string;
-}
-
-const CategorySchema = new mongoose.Schema<Categories>({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    max: 32,
-    unique: true,
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
   },
-  photo: {
-    data: Buffer,
-    contentType: String,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.Category ||
-  mongoose.model<Categories>("Category", CategorySchema);
+  mongoose.model("Category", categorySchema);
