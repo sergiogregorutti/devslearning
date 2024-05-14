3ac73c4ecebcbbfac49731059763619a363760cd
Sergio Gregorutti <sergiogregorutti@gmail.com>
2024-05-14 00:47:04 -0300

    Add active state to menu navigation

--- app/[lang]/(admin)/admin/ui/header/Header.tsx
+++ app/[lang]/(admin)/admin/ui/header/Header.tsx
@@ -1,19 +1,34 @@
 "use client";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 import Link from "next/link";
+import clsx from "clsx";
 import { getLocalizedPathFromPrefix } from "@/lib/language";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 import "./styles.css";
 
 const Header = ({ lang, user }: { lang: string; user: any }) => {
+  const pathname = usePathname();
   const isAuth = user ? user : false;
   const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   const toggleMobileMenu = () => {
     setShowMobileMenu(!showMobileMenu);
   };
+
+  const links = [
+    { name: "Dashboard", href: "/admin/", arrow: true },
+    {
+      name: "Technologies",
+      href: "/admin/technologies/",
+      arrow: true,
+    },
+    { name: "Courses", href: "/admin/courses/", arrow: true },
+    { name: "Go to Website", href: "/", arrow: false },
+  ];
 
   return (
     <header>
@@ -42,15 +57,21 @@ const Header = ({ lang, user }: { lang: string; user: any }) => {
                 </button>
               </div>
               <ul className="menu">
-                <li className="item">
-                  <Link href="/admin/technologies">Tecnologies</Link>
-                </li>
-                <li className="item">
-                  <Link href="/admin/courses">Courses</Link>
-                </li>
-                <li className="item">
-                  <Link href="/">Go to Website</Link>
-                </li>
+                {links.map((link) => {
+                  return (
+                    <li
+                      key={link.name}
+                      className={clsx("item", {
+                        active: pathname === link.href,
+                      })}
+                      onClick={toggleMobileMenu}
+                    >
+                      <Link href={link.href}>
+                        {link.name} {link.arrow ? <ArrowRightIcon /> : null}
+                      </Link>
+                    </li>
+                  );
+                })}
               </ul>
             </div>
             <img className="mobile-menu-logo" src="/assets/logo_dark.svg" />
--- app/[lang]/(admin)/admin/ui/navigation/Navigation.tsx
+++ app/[lang]/(admin)/admin/ui/navigation/Navigation.tsx
@@ -1,21 +1,40 @@
 "use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+import clsx from "clsx";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 import "./styles.css";
 
 const Navigation = ({}: {}) => {
+  const pathname = usePathname();
+
+  const links = [
+    { name: "Dashboard", href: "/admin/" },
+    {
+      name: "Technologies",
+      href: "/admin/technologies/",
+    },
+    { name: "Courses", href: "/admin/courses/" },
+  ];
+
   return (
     <div className="navigation">
       <ul className="menu">
-        <li className="item">
-          <Link href="/admin">Dashboard</Link>
-        </li>
-        <li className="item">
-          <Link href="/admin/technologies">Technologies</Link>
-        </li>
-        <li className="item">
-          <Link href="/admin/courses">Courses</Link>
-        </li>
+        {links.map((link) => {
+          return (
+            <li
+              key={link.name}
+              className={clsx("item", {
+                active: pathname === link.href,
+              })}
+            >
+              <Link href={link.href}>
+                {link.name} <ArrowRightIcon />
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
