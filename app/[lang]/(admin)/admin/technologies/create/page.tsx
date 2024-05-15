b01c7ab67d473ebfce858541810ede1c91ec3f72
Sergio Gregorutti <sergiogregorutti@gmail.com>
2024-05-15 12:56:41 -0300

    Improve styles

--- app/[lang]/(admin)/admin/technologies/create/page.tsx
+++ app/[lang]/(admin)/admin/technologies/create/page.tsx
@@ -1,8 +1,17 @@
+import { TextField } from "@mui/material";
+
 export default async function Page() {
   return (
     <>
       <h1>Technologies</h1>
       <h2>Create</h2>
+      <form>
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" name="name" className="form-input" />
+        <TextField fullWidth type="email" className="form-input" />
+      </form>
     </>
   );
 }
