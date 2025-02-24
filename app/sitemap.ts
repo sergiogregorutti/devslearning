33cf2cdec055aebffb40743221c7ac20b4cf1fa5
Sergio Gregorutti <sergiogregorutti@gmail.com>
2025-02-24 20:45:53 -0300

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,72 +12,72 @@ interface ICourse {
 
 const technologies: ITechnology[] = await fetchTechnologies();
 const technologyRoutesEn: any = technologies.map(
-  (tech) => `https://www.devslearning.com/technologies/${tech.slug}`
+  (tech) => `https://www.devslearning.com/technologies/${tech.slug}/`
 );
 const technologyRoutesEs: any = technologies.map(
-  (tech) => `https://www.devslearning.com/es/technologies/${tech.slug}`
+  (tech) => `https://www.devslearning.com/es/technologies/${tech.slug}/`
 );
 const technologyCoursesRoutesEn: any = technologies.map(
-  (tech) => `https://www.devslearning.com/technologies/${tech.slug}/courses`
+  (tech) => `https://www.devslearning.com/technologies/${tech.slug}/courses/`
 );
 const technologyCoursesRoutesEs: any = technologies.map(
-  (tech) => `https://www.devslearning.com/es/technologies/${tech.slug}/courses`
+  (tech) => `https://www.devslearning.com/es/technologies/${tech.slug}/courses/`
 );
 
 const courses: ICourse[] = await fetchCourses();
 const coursesRoutesEn: any = courses.map(
-  (course) => `https://www.devslearning.com/courses/${course._id}`
+  (course) => `https://www.devslearning.com/courses/${course._id}/`
 );
 const coursesRoutesEs: any = courses.map(
-  (course) => `https://www.devslearning.com/es/courses/${course._id}`
+  (course) => `https://www.devslearning.com/es/courses/${course._id}/`
 );
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.devslearning.com",
+      url: "https://www.devslearning.com/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: "https://www.devslearning.com/es",
+      url: "https://www.devslearning.com/es/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
-      url: "https://www.devslearning.com/technologies",
+      url: "https://www.devslearning.com/technologies/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
-      url: "https://www.devslearning.com/es/technologies",
+      url: "https://www.devslearning.com/es/technologies/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
-      url: "https://www.devslearning.com/courses",
+      url: "https://www.devslearning.com/courses/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
-      url: "https://www.devslearning.com/es/courses",
+      url: "https://www.devslearning.com/es/courses/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
-      url: "https://www.devslearning.com/about",
+      url: "https://www.devslearning.com/about/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
-      url: "https://www.devslearning.com/es/about",
+      url: "https://www.devslearning.com/es/about/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
