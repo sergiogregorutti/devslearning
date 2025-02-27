8e85a5a11064485ed6044dc0df320fb3b2ff9636
Sergio Gregorutti <sergiogregorutti@gmail.com>
2025-02-27 19:33:54 -0300

    Improve PageHeader styles

--- components/layout/PageHeader.tsx
+++ components/layout/PageHeader.tsx
@@ -17,22 +17,34 @@ export default function PageHeader({
 }: PageHeaderProps) {
   return (
     <section className="mt-12 sm:mt-18 lg:mt-22 mb-6 lg:mb-10">
-      <Container className={image ? "sm:grid sm:grid-cols-2 sm:gap-10" : ""}>
-        {image && (
-          <Image
-            src={image}
-            width={420}
-            height={308}
-            alt={title}
-            priority={true}
-            className="w-full mb-6 sm:mb-0 sm:order-2 max-h-[200px] sm:max-h-[300px]"
-          />
-        )}
-        <div className="sm:order-1">
-          <Title label={title} />
-          {description && (
-            <p className="mt-3 md:mt-8 xl:mt-10">{description}</p>
+      <Container>
+        <div
+          className={
+            image
+              ? "md:grid sm:grid-cols-[60%_40%] lg:grid-cols-[70%_30%] md:gap-[20px]"
+              : ""
+          }
+        >
+          {image && (
+            <div className="mb-3 sm:mb-0 md:order-2 md:flex md:justify-center">
+              <Image
+                src={image}
+                width={420}
+                height={308}
+                alt={title}
+                priority={true}
+                className="h-[130px] w-auto md:h-auto md:w-[80%] md:max-h-[300px]"
+              />
+            </div>
           )}
+          <div className="sm:order-1 sm:flex sm:flex-col">
+            <Title label={title} className="mb-3 md:mb-0" />
+            {description && (
+              <p className="text-gray-500 text-sm md:text-base md:grow md:flex md:items-center">
+                {description}
+              </p>
+            )}
+          </div>
         </div>
       </Container>
     </section>
--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -6,7 +6,8 @@ interface TitleProps {
 }
 
 export default function Title({ label, className = "" }: TitleProps) {
-  const baseStyles = "text-blue-800 text-4xl/10 sm:text-5xl/20 font-bold";
+  const baseStyles =
+    "text-blue-800 text-4xl/10 md:text-5xl/20 lg:text-6xl/20 font-bold";
 
   const titleClasses = `${baseStyles} ${className}`;
 
