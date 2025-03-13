ac806355b44d2ffa5deeddb48b72cfc3861739d5
Sergio Gregorutti <sergiogregorutti@gmail.com>
2025-03-13 19:43:13 -0300

    Migrate About page styles to TailwindCSS

--- components/pages/about/index.tsx
+++ components/pages/about/index.tsx
@@ -6,15 +6,13 @@ import Image from "next/image";
 import PageHeader from "@/components/layout/PageHeader";
 import Container from "@/components/layout/Container";
 
-import "./styles.css";
-
 const About: React.FC = () => {
   const { dictionary } = useLanguage();
 
   return (
     <>
       <PageHeader title={dictionary.about.title} />
-      <Container className="about-page">
+      <Container>
         <h2>{dictionary.about.mission}</h2>
         <p className="mb-6 leading-[30px]">
           {dictionary.about.mission_description}
@@ -24,27 +22,30 @@ const About: React.FC = () => {
           {dictionary.about.vision_description}
         </p>
         <h2>{dictionary.about.team}</h2>
-        <div className="team">
-          <div className="card">
+        <div className="grid grid-cols-1 gap-8 mt-6 md:grid-cols-2 lg:grid-cols-4">
+          <div className="flex flex-col items-center text-center bg-blue-100 rounded-[40px] p-8">
             <Image
               src="/assets/team/sergio.jpg"
               width={200}
               height={200}
               alt="Sergio Gregorutti"
-              className="image"
+              className="rounded-full"
             />
-            <h3>Sergio Gregorutti</h3>
-            <span className="position">
+            <h3 className="font-bold text-2xl text-blue-900 !mt-4 !mb-2">
+              Sergio Gregorutti
+            </h3>
+            <span className="block text-lg text-gray-500">
               {dictionary.about.team_members.sergio.role}
             </span>
-            <span className="description">
+            <span className="text-gray-700 mt-2">
               {dictionary.about.team_members.sergio.professionalProfile}
             </span>
-            <ul className="contact-info">
+            <ul className="flex grow items-end justify-center gap-4 mt-4">
               <li>
                 <Link
                   href="https://github.com/sergiogregorutti"
                   target="_blank"
+                  className="flex items-center gap-2"
                 >
                   <Image
                     src="/assets/icons/github.svg"
@@ -59,6 +60,7 @@ const About: React.FC = () => {
                 <Link
                   href="https://www.linkedin.com/in/sergiogregorutti/"
                   target="_blank"
+                  className="flex items-center gap-2"
                 >
                   <Image
                     src="/assets/icons/linkedin.svg"
@@ -71,26 +73,29 @@ const About: React.FC = () => {
               </li>
             </ul>
           </div>
-          <div className="card">
+          <div className="flex flex-col items-center text-center bg-blue-100 rounded-[40px] p-8">
             <Image
               src="/assets/team/florencia.jpeg"
               width={200}
               height={200}
               alt="Florencia De Luca"
-              className="image"
+              className="rounded-full"
             />
-            <h3>Florencia De Luca</h3>
-            <span className="position">
+            <h3 className="font-bold text-2xl text-blue-900 !mt-4 !mb-2">
+              Florencia De Luca
+            </h3>
+            <span className="block text-lg text-gray-500">
               {dictionary.about.team_members.florencia.role}
             </span>
-            <span className="description">
+            <span className="text-gray-700 mt-2">
               {dictionary.about.team_members.florencia.professionalProfile}
             </span>
-            <ul className="contact-info">
+            <ul className="flex grow items-end justify-center gap-4 mt-4">
               <li>
                 <Link
                   href="https://www.linkedin.com/in/florencia-de-luca-87573227/"
                   target="_blank"
+                  className="flex items-center gap-2"
                 >
                   <Image
                     src="/assets/icons/linkedin.svg"
