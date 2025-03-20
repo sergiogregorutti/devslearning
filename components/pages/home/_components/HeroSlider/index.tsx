73b080bff1b088bd61b4adf47c395c798ddc211f
Sergio Gregorutti <sergiogregorutti@gmail.com>
2025-03-20 01:05:21 -0300

    Migrate Hero Slider to TailwindCSS

--- components/pages/home/_components/HeroSlider/index.tsx
+++ components/pages/home/_components/HeroSlider/index.tsx
@@ -2,135 +2,61 @@
 
 import { useLanguage } from "@/components/context/LanguageContext";
 import React from "react";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
 import Image from "next/image";
 import Container from "@/components/layout/Container";
 import Button from "@/components/ui/Button";
 import { getLocalizedPathFromPrefix } from "@/lib/language";
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./styles.css";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function HeroSlider() {
   const { lang, dictionary } = useLanguage();
 
-  var sliderSettings = {
-    arrows: false,
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 6000,
-    speed: 800,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
-    <div className="hero-slider bg-blue-300 pb-10">
-      <Container>
-        <Slider {...sliderSettings}>
-          <div>
-            <div className="content">
-              <div className="col-text">
-                <span className={`title ${lang}`}>
-                  {dictionary.home.heroSlider[0].title}
-                </span>
-                <span className={`title2 ${lang}`}>
-                  {dictionary.home.heroSlider[0].title2}
-                </span>
-                <p>{dictionary.home.heroSlider[0].subtitle}</p>
+    <div className="bg-blue-300 pb-2 md:py-0 h-[calc(100vh_-_60px)] md:h-auto">
+      <Container className="h-[calc(100vh_-_60px)] md:h-auto">
+        <Swiper
+          modules={[Autoplay, Pagination]}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{ delay: 6000, disableOnInteraction: true }}
+          pagination={{ clickable: true }}
+          className="h-[calc(100vh_-_60px)] md:h-auto !pb-6 !py-3 lg:!pb-6 lg:!py-0"
+        >
+          {dictionary.home.heroSlider.map((slide: any, index: number) => (
+            <SwiperSlide key={index}>
+              <div className="grid grid-cols-1 md:grid-cols-[55%_45%]">
+                <div className="flex flex-col items-start justify-center order-2 md:order-1">
+                  <span className="text-blue-800 text-4xl md:text-5xl lg:text-6xl font-bold leading-[50px] mb-3 md:pr-[30%]">
+                    {slide.title}
+                  </span>
+                  <p className="mb-3 md:pr-[20%]">{slide.subtitle}</p>
+                  {slide.cta && (
+                    <Button
+                      label={slide.cta}
+                      href={getLocalizedPathFromPrefix(lang, slide.link)}
+                    />
+                  )}
+                </div>
+                <div className="flex flex-col items-center justify-center order-1 md:order-2 pb-6 md:py-0">
+                  <Image
+                    src={`/assets/${slide.image}`}
+                    width={555}
+                    height={286}
+                    alt="Devs Learning"
+                    priority={true}
+                    className={`${
+                      slide.imageNoMaxHeigh ? "" : "max-h-[100%] md:max-h-[80%]"
+                    }`}
+                  />
+                </div>
               </div>
-              <div className="col-image">
-                <Image
-                  src="/assets/boy.svg"
-                  width={555}
-                  height={286}
-                  alt="Devs Learning"
-                  priority={true}
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="content">
-              <div className="col-text">
-                <span className={`title ${lang}`}>
-                  {dictionary.home.heroSlider[1].title}
-                </span>
-                <span className={`title2 ${lang}`}>
-                  {dictionary.home.heroSlider[1].title2}
-                </span>
-                <p>{dictionary.home.heroSlider[1].subtitle}</p>
-                <Button
-                  label={dictionary.home.heroSlider[1].cta}
-                  href={getLocalizedPathFromPrefix(lang, `/courses`)}
-                />
-              </div>
-              <div className="col-image">
-                <Image
-                  src="/assets/boy2.svg"
-                  width={555}
-                  height={286}
-                  alt="Devs Learning"
-                  priority={true}
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="content">
-              <div className="col-text">
-                <span className={`title ${lang}`}>
-                  {dictionary.home.heroSlider[2].title}
-                </span>
-                <span className={`title2 ${lang}`}>
-                  {dictionary.home.heroSlider[2].title2}
-                </span>
-                <p>{dictionary.home.heroSlider[2].subtitle}</p>
-                <Button
-                  label={dictionary.home.heroSlider[2].cta}
-                  href={getLocalizedPathFromPrefix(lang, `/technologies`)}
-                />
-              </div>
-              <div className="col-image">
-                <Image
-                  src="/assets/man_working.svg"
-                  width={555}
-                  height={286}
-                  alt="Devs Learning"
-                  priority={true}
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="content">
-              <div className="col-text">
-                <span className={`title ${lang}`}>
-                  {dictionary.home.heroSlider[3].title}
-                </span>
-                <span className={`title2 ${lang}`}>
-                  {dictionary.home.heroSlider[3].title2}
-                </span>
-                <p>{dictionary.home.heroSlider[3].subtitle}</p>
-                <Button
-                  label={dictionary.home.heroSlider[3].cta}
-                  href={getLocalizedPathFromPrefix(lang, `/roadmaps`)}
-                />
-              </div>
-              <div className="col-image">
-                <Image
-                  src="/assets/girl.svg"
-                  width={555}
-                  height={286}
-                  alt="Devs Learning"
-                  priority={true}
-                />
-              </div>
-            </div>
-          </div>
-        </Slider>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Container>
     </div>
   );
